31110eab1f854f9ac2756623739c2a301707bee8
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2023-08-20 13:40:59 +1000

    🔧 chore(.gitignore): ignore storybook-static directory to prevent committing storybook build files 🔧 chore(.storybook/main.ts): update storybook configuration to improve storybook usage 🔧 chore(package.json): update package name to 'personal-blog-template' for better identification ⬆️ chore(package.json): upgrade next, react, and react-dom versions to get latest features and bug fixes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,22 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)"],
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "@storybook/addon-designs",
   ],
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
   docs: {
-    autodocs: true,
+    autodocs: "tag",
   },
 };
 export default config;
