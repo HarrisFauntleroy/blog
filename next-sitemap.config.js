44e57efa5f04e063a47213989d74e10846a609e0
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2023-09-17 13:50:58 +1000

    🔧 chore(github workflows): simplify workflows by removing node-version matrix and using bun for dependency management 🌐 feat(next-sitemap.config.js): add sitemap configuration for SEO optimization 🔧 chore(package.json): add file-progress plugin to ESLint and update eslint-plugin-prettier version 📦 feat(package.json): add next-sitemap package for generating sitemap 🔄 fix(package.json): correct the order of typescript and ts-jest in devDependencies

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+const NextSitemapConfig = {
+  siteUrl: "https://personal-blog-template-lac.vercel.app/",
+  generateRobotsTxt: true,
+};
+
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = NextSitemapConfig;
